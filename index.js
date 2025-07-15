959ac3e3c290c4c7210fa0b9492eb1b295609714
MahendraMer <mahendramer0000@gmail.com>
2025-07-15 10:57:47 +0530

    even odd complete

---
+++ index.js
@@ -0,0 +1,17 @@
+// const name = "Mahendra Mer"
+// console.log("Hello,"+name)
+// console.log(`Hello How Are You ${name}`)
+
+
+// const n = 32
+// if(n%2==0)
+//     {
+//        console.log("even number")
+//     }
+// else
+//     {
+//         console.log("odd number")
+//     }
+
+
+const num-1 = 
